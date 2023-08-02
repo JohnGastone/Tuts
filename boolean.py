b32d7db92395cda4a86785dd071a82a10146a60e
JohnGastone <johngastone11@gmail.com>
2023-08-02 23:34:31 +0300

    Session 6

---
+++ boolean.py
@@ -0,0 +1,36 @@
+# This file is for boolean manipulations
+
+# Comparing numbers in python
+
+
+y = 5
+
+
+if y < 12:
+    print("Anna is a mathematician")
+else:
+    print("Anna is a winga at Kariakoo")
+
+
+# str for string, int for integers, bool for booleans
+
+print(bool("Tanzania"))
+print(bool())
+
+
+'''
+
+Most Values are True
+Almost any value is evaluated to True if it has some sort of content.
+
+Any string is True, except empty strings.
+
+Any number is True, except 0.
+
+Any list, tuple, set, and dictionary are True, except empty ones.
+
+'''
+
+
+# print([])
+print(bool({}))
---
+++ operators.py
@@ -0,0 +1,62 @@
+# This file is for the operator computations
+# Python operators are used to perfom operations on variables or values
+
+# Types of Python Operators
+# 1. Arithmetic Operators
+# 2. Logical Operators
+# 3. Assignment Operators
+# 4. Comparison Operators
+# 5. Identity Operators
+# 6. Membership Operators
+# 7. Bitwise Operators
+
+
+# Arithmetic Operators
+# Addition (+)
+x = 4
+y = 6
+
+z = x + y
+print(z)
+# Subtraction (-)
+
+a = y - x
+print(a)
+# Multiplication (*)
+
+j = x * y
+print(j)
+# Division (/)
+
+k = x / y
+print(k)
+
+# Exponentiation (**)
+
+r = x**2
+f = x**3
+
+# Modulus (%)
+i = y % x
+print(i)
+
+
+# Comparison Operators
+# Greater than (>)
+print(x > y)
+
+# Less than (<)
+print(x < y)
+
+# Equal (==)
+print(x == y)
+
+# Not equal (!=)
+print(x != y)
+
+
+# Greater than or equal (>=)
+print(x >= y)
+
+# Less than or equal ( <=)
+print(x <= y)
