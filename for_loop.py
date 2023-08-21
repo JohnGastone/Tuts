a19cbd5f66a1f82e2ecf046a8e4fb398402d2f7b
JohnGastone <johngastone11@gmail.com>
2023-08-21 21:36:22 +0300

    ++

--- for_loop.py
+++ for_loop.py
@@ -4,3 +4,10 @@
 x = ('Anna', 'Maokoto', 'Mwaisa')
 for item in x:
     print(item)
+
+
+mapochopocho = ['Biryani', 'Pilau', 'Wali nazi', 'Makange', 'Kitimoto']
+# mapochopocho = masotojo.list()
+for pochopocho in mapochopocho:
+    print(pochopocho)
+    print("It is true this " + pochopocho + ' is amazing')
