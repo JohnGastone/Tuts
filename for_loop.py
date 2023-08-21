d1dd145f6aebd367c20231fa1694c21c4ef6a0d0
JohnGastone <johngastone11@gmail.com>
2023-08-21 21:38:25 +0300

    +++

--- for_loop.py
+++ for_loop.py
@@ -11,3 +11,13 @@ mapochopocho = ['Biryani', 'Pilau', 'Wali nazi', 'Makange', 'Kitimoto']
 for pochopocho in mapochopocho:
     print(pochopocho)
     print("It is true this " + pochopocho + ' is amazing')
+
+
+switch_dict = {
+    "case1": "This is case 1",  # This is the first case of the Switch statement
+    "case2": "This is case 2",  # ----||-----
+    "case3": "This is case 3"  # ----||-----
+}
+
+for case in switch_dict:
+    print(case)
