1fe3456193b2e8d1c2020e60053954601392fdb3
JohnGastone <johngastone11@gmail.com>
2023-08-23 20:33:59 +0300

    +++

---
+++ taskQuestions.py
@@ -0,0 +1,32 @@
+'''
+Question 1:
+Write a Python program to print the numbers from 1 to 10.
+
+Question 2:
+Write a Python program to calculate the sum of all even numbers from 1 to 50.
+
+Question 3:
+Write a Python program to generate the Fibonacci sequence of length 10.
+
+Question 4:
+Write a Python program to print a right-aligned pattern of stars for a given number of rows.
+
+Question 5:
+Write a Python program to calculate the factorial of a given number using a `for` loop.
+
+Question 6:
+Write a Python program to find the prime numbers between 1 and 100.
+
+Question 7:
+Write a Python program to reverse a given string using a `for` loop.
+
+Question 8:
+Write a Python program to check if a given word is a palindrome or not.
+
+Question 9:
+Write a Python program to calculate the average of a list of numbers using a `for` loop.
+
+Question 10:
+Write a Python program that prints a multiplication table for a given number up to a specified range.
+    
+'''
