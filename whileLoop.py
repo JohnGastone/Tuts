daa9f75ecf24ed4c00682f7960517a4704fde118
JohnGastone <johngastone11@gmail.com>
2023-08-23 21:50:46 +0300

    +++

---
+++ whileLoop.py
@@ -0,0 +1,10 @@
+# While loop execute whenever the stated condition hold true
+
+# Declaration of While Loop
+
+r = 1
+while r < 10:
+    print("See while loop magic")
+    if r == 7:
+        break
+    r += 1
