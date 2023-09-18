e0c6f949f34515e62aedc9b1ba35346933424843
JohnGastone <johngastone11@gmail.com>
2023-09-18 21:09:29 +0300

    ++

---
+++ python_math.py
@@ -0,0 +1,46 @@
+# Python has a set of built-in math functions, including an extensive math module,
+# that allows you to perform mathematical tasks on numbers.
+
+
+# There exists a number of built-in functions for math in Python
+
+'''
+
+math.sqrt(x): Returns the square root of x.
+math.pow(x, y): Returns x raised to the power y.
+math.exp(x): Returns the exponential value of x (e^x).
+math.log(x[, base]): Returns the natural logarithm of x (base e), or the logarithm of x with the specified base if provided.
+math.log10(x): Returns the base-10 logarithm of x.
+math.ceil(x): Returns the smallest integer greater than or equal to x (ceiling function).
+math.floor(x): Returns the largest integer less than or equal to x (floor function).
+math.trunc(x): Returns the integer value of x (truncates the decimal part).
+math.sin(x): Returns the sine of x (in radians).
+math.cos(x): Returns the cosine of x (in radians).
+math.tan(x): Returns the tangent of x (in radians).
+math.asin(x): Returns the arcsine of x, in radians.
+math.acos(x): Returns the arccosine of x, in radians.
+math.atan(x): Returns the arctangent of x, in radians.
+math.degrees(x): Converts x from radians to degrees.
+math.radians(x): Converts x from degrees to radians.
+math.pi: A constant representing the mathematical constant π (pi).
+math.e: A constant representing the mathematical constant e.
+To use these functions, you'll need to import the math module like this:
+
+
+'''
+
+import math
+
+# Calculating area of a circle using math module
+radius = 7
+pi = 3.14
+Area = pi * radius * radius
+
+print("Area of a given circle is " + str(Area))
+
+
+# Calculating square root of a number using math module
+
+kipeo_cha_pili = math.sqrt(4)
+
+print("Square root of a given number is " + str(kipeo_cha_pili))
