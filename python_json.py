948c3c522badb0e59b9c37ea267c95112335a5d9
JohnGastone <johngastone11@gmail.com>
2023-09-18 21:13:38 +0300

    json

---
+++ python_json.py
@@ -0,0 +1,8 @@
+'''
+
+JSON (JavaScript Object Notation) is a lightweight data-interchange format that is easy 
+for humans to read and write and easy for machines to parse and generate. 
+In Python, you can work with JSON data using the built-in json module, which provides 
+functions for encoding and decoding JSON data.
+
+'''
