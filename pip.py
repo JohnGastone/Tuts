71eee49325f1d22340489bc17e350586dc32a67a
JohnGastone <johngastone11@gmail.com>
2023-09-20 21:46:45 +0300

    pip

---
+++ pip.py
@@ -0,0 +1,24 @@
+# PIP - Python package manager
+
+'''
+Package managers are tools used in programming to simplify the installation, 
+management, and distribution of libraries, dependencies, and software packages.
+Examples of package managers include
+
+{
+Javascript : npm package manager
+Python : pip package manager
+Dart : pub package manager
+Java : maven package manager
+}
+'''
+
+# Examples of packages/libraries in python
+
+'''
+1. Pandas.
+2. Matplotlib. - Plotting functions
+3. Scipy - Scientific computations i.e Physics
+4. PyPlot - plotting graphs
+
+'''
