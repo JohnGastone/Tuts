99041b986b41952e8df6d0eb494e9354721f623b
JohnGastone <johngastone11@gmail.com>
2023-10-10 19:33:29 +0300

    some code

---
+++ calculator.py
@@ -0,0 +1,30 @@
+x = 1000
+
+y = 119
+
+# Addition
+
+answer = x + y
+
+print(answer)
+
+
+# subtraction
+
+answer = x - y
+
+print(answer)
+
+# Multiplication
+
+answer = x * y
+
+print(answer)
+
+# Division
+
+answer = x / y
+
+print(answer)
+
+print("Hello Jovin David, how is your day?")
---
+++ task.go
@@ -0,0 +1,64 @@
+**Fractions:**
+
+1. What is the sum of 2/5 and 3/4?
+2. Subtract 1/3 from 2/3.
+3. Express 4/9 as a decimal.
+4. Multiply 1/8 by 5/6.
+5. Divide 2/3 by 4/5.
+6. Simplify the fraction 18/36 to its lowest terms.
+7. Express 3/8 as a percentage.
+8. Compare the fractions 5/7 and 3/5. Which one is larger?
+9. Convert the mixed number 2 1/4 to an improper fraction.
+10. Add 1/5 to 3/10.
+
+**Decimals:**
+
+11. Write 0.125 as a fraction in simplest form.
+12. Add 0.6 and 0.75.
+13. Subtract 1.25 from 3.5.
+14. Multiply 0.4 by 0.5.
+15. Divide 2.4 by 0.6.
+16. Express 5/8 as a decimal.
+17. Compare the decimals 0.875 and 0.9. Which one is larger?
+18. Convert the fraction 3/4 to a decimal.
+19. Calculate 15% of 240.
+20. Write 7.5 as a mixed number.
+
+**Ratios:**
+
+21. If the ratio of boys to girls in a class is 3:5, and there are 24 students in total, how many girls are there?
+22. Simplify the ratio 20:25 to its lowest terms.
+23. If a recipe calls for a ratio of 2 cups of flour to 3 cups of sugar, how much sugar should you use if you have 4 cups of flour?
+24. If the length of a rectangle is 8 cm and the width is in a ratio of 4:5, what is the width?
+25. A bag contains 4 red marbles, 3 blue marbles, and 5 green marbles. What is the ratio of red to blue marbles?
+26. If a map scale is 1:10,000, and the distance between two cities on the map is 5 cm, what is the actual distance between the cities in kilometers?
+27. Divide $80 in the ratio of 3:5 between two friends. How much does each friend receive?
+28. The ratio of boys to girls on a soccer team is 7:5. If there are 24 players on the team, how many are boys?
+29. If 15% of a solution is salt, what is the ratio of salt to the total solution?
+
+**Percentages:**
+
+30. Express 25% as a decimal.
+31. What is 40% of 120?
+32. If a shirt originally costs $50 and is on sale for 20% off, what is the sale price?
+33. If you scored 85 out of 100 on a test, what is your percentage score?
+34. A store marks up the price of an item by 30%. If the item costs $40 before the markup, what is the new price?
+35. What percentage of 60 is 24?
+36. If you have a 10% tip on a restaurant bill of $45, how much is the tip?
+37. A store offers a 15% discount on all items. If a toy originally costs $20, what is the sale price?
+38. If 20% of a number is 40, what is the number?
+39. Express 3/4 as a percentage.
+
+**Mixed Practice:**
+
+40. A recipe calls for 2/3 cup of milk, but you only want to make half the recipe. How much milk should you use?
+41. If 30% of a class consists of boys, and there are 40 students in the class, how many boys are there?
+42. Subtract 1.75 from 4.25.
+43. Write 5.6 as a mixed number.
+44. What is 120% of 75?
+45. Simplify the expression: 3/4 + 1/6.
+46. If a car travels 180 miles on 6 gallons of gas, what is its miles per gallon (MPG)?
+47. Calculate 25% of 80.
+48. A shirt costs $25, and you have a 15% coupon. What is the final price?
+49. If 25% of a solution is acid, what is the ratio of acid to the total solution?
+50. A rectangle has a length of 10 cm and a width in a ratio of 2:3. What is the area of the rectangle in square centimeters?
