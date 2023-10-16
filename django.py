68cf479fcc85519fd64a3c3494f250196c3f6671
JohnGastone <johngastone11@gmail.com>
2023-10-16 21:18:54 +0300

    Django

---
+++ django.py
@@ -0,0 +1,62 @@
+# Django is a:
+'''
+1. Python framework.
+2. Open source.
+3. Web framework.
+4. High level.
+5. Rapid development tool.
+
+'''
+# Key Concepts :
+'''
+1. It has an MVT architectural pattern. Model-View-Template
+2. It has a built-in admin dashboard.
+3. It employs Django Template Language(DTL)
+
+'''
+
+# Strengths :
+
+'''
+1. Rapid development.
+2. It has a built-in admin dashboard.
+3. Scalability.
+4. Community support.
+5. Security (SQL injection, cross-site request forgery(CSRF), Cross-site scripting(XSS)).
+
+'''
+
+# Weaknesses :
+
+'''
+1. Learning Curve.
+2. Performance.
+3. Flexibility.
+
+'''
+
+# Django installation and Project execution
+
+'''
+Prerequisites :
+1. Python installed.
+2. Web and web development skills.
+3. Version control skills.
+
+
+Virtual Environment :  is a self-contained directory that houses a specific 
+Python interpreter and a set of libraries and packages.
+VE Functions: 
+- Dependency isolation
+- Avoiding version conflicts
+- Easy dependencies management
+
+Steps :
+1. Install python
+2. Create a Virtual Environment
+3. Activate virtual environment.
+4. Install Django.
+5. Verify installation.
+
+
+'''
